50dc1caf01259ae2945c65b5e34b0c7720519102
Rajesh000730 <rajeshrachit09@gmail.com>
2025-06-07 15:32:49 +0530

    modify: createuser user route

--- routes/routes.go
+++ routes/routes.go
@@ -9,6 +9,8 @@ import (
 func RegisterRoutes(r *gin.Engine) {
 	auth := r.Group("/auth")
 	auth.POST("/loginuser", controllers.LoginUserHandler)
+	auth.POST("/createuser", controllers.RegisterUserHandler)
+
 	api := r.Group("/api")
 	r.GET("/", controllers.HomeHandler)
 	api.Use(middlewares.AuthMiddleware())
@@ -17,7 +19,6 @@ func RegisterRoutes(r *gin.Engine) {
 		api.GET("/aptitudequestion", controllers.GetAptitudeQuestions)
 		api.GET("/ppequestion", controllers.GetPpeQuestions)
 		api.POST("/ppescore", controllers.GetPpeScore)
-		api.POST("/createuser", controllers.RegisterUserHandler)
 		api.GET("/getuserinfo", controllers.GetUserInfo)
 		api.POST("/logoutuser", controllers.LogoutHandler)
 		api.GET("/topscore", controllers.GetTopScore)
